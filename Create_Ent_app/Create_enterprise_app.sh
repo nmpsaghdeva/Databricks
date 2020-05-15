e8e9aa813916155b00a8cfedf7bb387752db9485
nmpsaghdeva <60475282+nmpsaghdeva@users.noreply.github.com>
2020-05-15 15:30:56 +0530

    Add files via upload

--- Create_Ent_app/Create_enterprise_app.sh
+++ Create_Ent_app/Create_enterprise_app.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-if [ $# != 1 ]
+if [ $# != 3 ]
 then
-  echo "Usage: $0 Enter Name of the Application "
+  echo "Usage: $0 <Enter Name of the Application> <Application Admin Username> <Password> "
   exit
 fi
+rm -f ./para.json
 echo "{"  > ./para.json
 echo "\"displayName\": \"$1\"" >> ./para.json
 echo "}\n" >> ./para.json
+echo $2
+echo $3
+
+az login --username $2 --password $3
 response=$(az account get-access-token --resource-type ms-graph)
 token=$(echo $response | jq ".accessToken" -r)
 curl -X POST -H "Authorization: Bearer $token" -d @para.json https://graph.microsoft.com/beta/applicationTemplates/9c9818d2-2900-49e8-8ba4-22688be7c675/instantiate  -H "Content-Type: application/json" |jq
