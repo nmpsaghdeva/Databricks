8841b10544790b203f39bb0b7adce7952ee349d9
nmpsaghdeva <60475282+nmpsaghdeva@users.noreply.github.com>
2020-05-22 17:46:54 +0530

    Added Service principal

--- Create_Ent_app/Create_enterprise_app.sh
+++ Create_Ent_app/Create_enterprise_app.sh
@@ -12,7 +12,8 @@ echo "}\n" >> ./para.json
 echo $2
 echo $3
 
-az login --username $2 --password $3
+az login --service-principal --username 4ae3d5ed-bbd0-485d-956b-19c0378940a2 --password 0e9ca1ef-d135-4bd4-8968-f7c0af168a56 --tenant bc111fcd-1154-4322-b437-799c66a7677c
+#az login --username $2 --password $3
 response=$(az account get-access-token --resource-type ms-graph)
 token=$(echo $response | jq ".accessToken" -r)
 curl -X POST -H "Authorization: Bearer $token" -d @para.json https://graph.microsoft.com/beta/applicationTemplates/9c9818d2-2900-49e8-8ba4-22688be7c675/instantiate  -H "Content-Type: application/json" |jq
