77a1cb855b236df6a28d9f3a62ed57639d5b6eec
nmpsaghdeva <60475282+nmpsaghdeva@users.noreply.github.com>
2020-05-22 18:01:42 +0530

    Added parrameters

--- Create_Ent_app/Create_enterprise_app.sh
+++ Create_Ent_app/Create_enterprise_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# != 3 ]
+if [ $# != 4 ]
 then
   echo "Usage: $0 <Enter Name of the Application> <Application Admin Username> <Password> "
   exit
@@ -12,7 +12,7 @@ echo "}\n" >> ./para.json
 echo $2
 echo $3
 
-az login --service-principal --username 4ae3d5ed-bbd0-485d-956b-19c0378940a2 --password 0e9ca1ef-d135-4bd4-8968-f7c0af168a56 --tenant bc111fcd-1154-4322-b437-799c66a7677c
+az login --service-principal --username $2 --password $3 --tenant $4
 #az login --username $2 --password $3
 response=$(az account get-access-token --resource-type ms-graph)
 token=$(echo $response | jq ".accessToken" -r)
